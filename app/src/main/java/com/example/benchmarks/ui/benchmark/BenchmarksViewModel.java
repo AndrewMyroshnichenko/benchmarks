9c2299d62d340367f6971e50fdfef0894dafa7c9
Andrew Miroshnichenko <mobilemiron@gmail.com>
2022-11-05 01:22:02 +0200

    BenchmarksViewModel.java and OperationsCollections.java not ready yet, I need some time for finishing them

--- app/src/main/java/com/example/benchmarks/ui/benchmark/BenchmarksViewModel.java
+++ app/src/main/java/com/example/benchmarks/ui/benchmark/BenchmarksViewModel.java
@@ -4,7 +4,16 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/* This class is not finished
+I am only thinking how to realize logic of creating Threads and mark duration of operations
+ */
+
 public class BenchmarksViewModel extends ViewModel {
+    List<String> collections = new ArrayList<>();
+    List<String> operations = new ArrayList<>();
 
     public static boolean isNumberCorrect(String number){
         long temp;
@@ -15,5 +24,14 @@ public class BenchmarksViewModel extends ViewModel {
         }
         return (temp > 0);
     }
+
+    public void startProcess(){
+        for (int i = 0; i < collections.size(); i++) {
+            for (int j = 0; j < operations.size(); j++) {
+                new Thread(new OperationsCollections(100000)).start();
+            }
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/example/benchmarks/ui/benchmark/OperationsCollections.java
@@ -0,0 +1,81 @@
+package com.example.benchmarks.ui.benchmark;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/* This class is not finished
+I am only thinking how to realize logic of creating Threads and mark duration of operations
+ */
+
+public class OperationsCollections implements Runnable {
+
+    private final long sizeOfCollection;
+
+    public OperationsCollections(long sizeOfCollection) {
+        this.sizeOfCollection = sizeOfCollection;
+    }
+
+    public List<Integer> createCollection(int numberOfCollection){
+        List<Integer> list;
+        switch (numberOfCollection){
+            case 1:
+                list = new LinkedList<Integer>();
+                fillCollection(list);
+                break;
+            case 2:
+                list = new CopyOnWriteArrayList<Integer>();
+                fillCollection(list);
+                break;
+            default:
+                list = new ArrayList<Integer>();
+                fillCollection(list);
+                break;
+        }
+        return list;
+    }
+
+    public long markTheTimeOperation(List<Integer> list, int numberOfOperation){
+        long time = 0;
+        final int valueForSearching = 200;
+        long startTime = System.nanoTime();
+        switch (numberOfOperation){
+            case 0:
+                list.add(0, (int) (Math.random() * 100));
+                break;
+            case 1:
+                list.add(list.size() / 2, valueForSearching);
+                break;
+            case 2:
+                list.add((int) (Math.random() * 100));
+                break;
+            case 3:
+                list.indexOf(valueForSearching);
+                break;
+            case 4:
+                list.remove(0);
+                break;
+            case 5:
+                list.remove(list.size() / 2);
+                break;
+            case 6:
+                list.remove(list.size() - 1);
+                break;
+        }
+        long endTime = System.nanoTime();
+
+        return endTime - startTime;
+    }
+
+    private void fillCollection(List<Integer> listOfCollection){
+        for (int i = 0; i < sizeOfCollection; i++) {
+            listOfCollection.add((int) (Math.random() * 100));
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
