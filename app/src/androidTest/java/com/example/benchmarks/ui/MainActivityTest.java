d2619e406316b6d975a16a21a59616dc2872e88d
Andrew Miroshnichenko <mobilemiron@gmail.com>
2023-03-23 18:48:11 +0200

    test: delete redundant imports from MainActivityTest.java

--- app/src/androidTest/java/com/example/benchmarks/ui/MainActivityTest.java
+++ app/src/androidTest/java/com/example/benchmarks/ui/MainActivityTest.java
@@ -9,12 +9,9 @@ import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.content.Context;
-
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.benchmarks.R;
 
