2e5362aab52d6f9817f2833995b4645dbe9cd387
Andrew Miroshnichenko <mobilemiron@gmail.com>
2023-04-10 00:35:04 +0300

    migration: BenchmarksTypesAdapter to kotlin

---
+++ app/src/main/java/com/example/benchmarks/ui/BenchmarkTypesAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.benchmarks.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.benchmarks.ui.benchmark.BenchmarkFragment
+
+class BenchmarkTypesAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, private val nameOfTabs: List<String>) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return nameOfTabs.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return BenchmarkFragment.createFragment(position)
+    }
+}
