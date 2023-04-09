40a8b39f3e9113b053aabec1ffceac9a98bf2ffb
Andrew Miroshnichenko <mobilemiron@gmail.com>
2023-04-10 00:24:13 +0300

    migration: BenchmarksModule to kotlin

---
+++ app/src/main/java/com/example/benchmarks/models/BenchmarksModule.kt
@@ -0,0 +1,26 @@
+package com.example.benchmarks.models
+
+import com.example.benchmarks.models.benchmark.Benchmark
+import com.example.benchmarks.models.benchmark.OperationsCollections
+import com.example.benchmarks.models.benchmark.OperationsMaps
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+
+
+@Module
+open class BenchmarksModule {
+
+    @Provides
+    @Named("collections")
+    open fun provideOperationsCollections(): Benchmark{
+        return OperationsCollections()
+    }
+
+    @Provides
+    @Named("maps")
+    open fun provideOperationMaps(): Benchmark{
+        return OperationsMaps()
+    }
+
+}
