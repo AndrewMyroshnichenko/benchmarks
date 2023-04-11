03ed19d8eecd53da3624ab28e4c9ecf034327c96
Andrew Miroshnichenko <mobilemiron@gmail.com>
2023-04-11 20:50:33 +0300

    refactor: MainActivity.kt changed !! to let.

--- app/src/main/java/com/example/benchmarks/ui/MainActivity.kt
+++ app/src/main/java/com/example/benchmarks/ui/MainActivity.kt
@@ -11,17 +11,21 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity(), OnTabSelectedListener {
 
-    private var bind : ActivityMainBinding? = null
-    private var tabLayoutMediator : TabLayoutMediator? = null
+    private var bind: ActivityMainBinding? = null
+    private var tabLayoutMediator: TabLayoutMediator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind?.root)
-        val nameOfTabs : List<String> = resources.getStringArray(R.array.name_tabs).toList()
+        val nameOfTabs: List<String> = resources.getStringArray(R.array.name_tabs).toList()
         bind?.mainViewPager?.adapter = BenchmarkTypesAdapter(supportFragmentManager, lifecycle, nameOfTabs)
-        tabLayoutMediator = TabLayoutMediator(bind!!.mainTabLayout, bind!!.mainViewPager) { tab, position ->
-            tab.text = nameOfTabs[position]
+
+        tabLayoutMediator = bind?.let { bind ->
+            TabLayoutMediator(bind.mainTabLayout, bind.mainViewPager)
+            { tab, position ->
+                tab.text = nameOfTabs[position]
+            }
         }
         tabLayoutMediator?.attach()
         bind?.mainTabLayout?.addOnTabSelectedListener(this)
@@ -33,11 +37,17 @@ class MainActivity : AppCompatActivity(), OnTabSelectedListener {
     }
 
     override fun onTabSelected(tab: TabLayout.Tab?) {
-        val id = when(bind?.mainTabLayout?.selectedTabPosition) {
+        val id = when (bind?.mainTabLayout?.selectedTabPosition) {
             0 -> R.drawable.tab_item_left_background
             else -> R.drawable.tab_item_right_background
         }
-        bind?.mainTabLayout?.setSelectedTabIndicator(ResourcesCompat.getDrawable(resources, id, theme))
+        bind?.mainTabLayout?.setSelectedTabIndicator(
+            ResourcesCompat.getDrawable(
+                resources,
+                id,
+                theme
+            )
+        )
     }
 
     override fun onTabUnselected(tab: TabLayout.Tab?) {
